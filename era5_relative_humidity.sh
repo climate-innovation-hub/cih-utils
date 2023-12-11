78f70a2edded53375320a18053e79421c5bdc786
Damien Irving <irving.damien@gmail.com>
2023-12-11 15:27:05 +1100

    Update path

--- era5_relative_humidity.sh
+++ era5_relative_humidity.sh
@@ -15,7 +15,7 @@ python=/g/data/xv83/dbi599/miniconda3/envs/npcp/bin/python
 for year in $(seq 1940 2022); do
     t2m_files=(/g/data/rt52/era5/single-levels/reanalysis/2t/${year}/*.nc)
     d2m_files=(/g/data/rt52/era5/single-levels/reanalysis/2d/${year}/*.nc)
-    outfile=/g/data/wp00/users/dbi599/observations/ERA5/${outvar}/daily/${outvar}_ERA-5_day_aus0.05_${year}.nc
+    outfile=/g/data/wp00/data/observations/ERA5/${outvar}/daily/${outvar}_ERA-5_day_aus0.05_${year}.nc
     command="${python} era5_relative_humidity.py ${outvar} ${outfile} --temperature_files ${t2m_files[@]} --dewpoint_files ${d2m_files[@]}"
     echo ${command}
     ${command}
